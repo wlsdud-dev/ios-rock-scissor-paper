e1c9273d560c0b9638c105e4771696963982006c
wlsdud-dev <hjymail1025@gmail.com>
2023-11-22 14:54:23 +0900

    feat : 컴퓨터의 선택 랜덤변수 추가, 종료 명령 추가(#2) 컴퓨터의 가위 바위 보 선택 변수를 생성하고 종료 기능을 추가 fix : input 변수에 예외처리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -9,13 +9,16 @@ import Foundation
 while true {
     print("가위(1) 바위(2) 보(3)! <종료 : 0> :")
     var input = readLine()
+    var comChoice = Int.random(in: 1...3)
     
-    switch Int(input) {
+    switch Int(input ?? "a") {
     case 1:
     case 2:
     case 3:
     case 0:
     default :
+        print("종료합니다")
+        break
     }
 }
 
