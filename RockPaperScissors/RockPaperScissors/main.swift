fa78c8f1b5acda5b5735bf4150b00650460a4931
wlsdud-dev <hjymail1025@gmail.com>
2023-11-22 15:03:35 +0900

    feat : 사용자 입력에 따른 기능 추가(#3) 사용자 입력에 따른 가위 바위 보 승패 결과를 걔산하고 출력 잘못된 입력 기능 추가 fix : 종료 기능 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,12 +13,34 @@ while true {
     
     switch Int(input ?? "a") {
     case 1:
+        if comChoice == 1 {
+            print("비겼습니다!")
+        } else if comChoice == 2 {
+            print("졌습니다!")
+        }else if comChoice == 3 {
+            print("이겼습니다!")
+        }
     case 2:
+        if comChoice == 2 {
+            print("비겼습니다!")
+        } else if comChoice == 3 {
+            print("졌습니다!")
+        }else if comChoice == 1 {
+            print("이겼습니다!")
+        }
     case 3:
+        if comChoice == 3 {
+            print("비겼습니다!")
+        } else if comChoice == 1 {
+            print("졌습니다!")
+        }else if comChoice == 2 {
+            print("이겼습니다!")
+        }
     case 0:
-    default :
         print("종료합니다")
         break
+    default :
+        print("잘못된 입력입니다. 다시 시도해주세요.")
     }
 }
 
