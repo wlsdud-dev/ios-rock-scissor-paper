ce738b3f871e0062c5965e475bd0095f4a002f17
wlsdud-dev <hjymail1025@gmail.com>
2023-11-27 11:22:03 +0900

    refactor : 가위바위보 게임 리팩토링
    기존 가위바위보 게임을 리팩토링 하기 위해 enum 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,46 +2,18 @@
 //  RockPaperScissors - main.swift
 //  Created by tacocat.
 //  Copyright © tastycode. All rights reserved.
-// 
+//
 
-import Foundation
-
-while true {
-    print("가위(1) 바위(2) 보(3)! <종료 : 0> :")
-    var input = readLine()
-    var comChoice = Int.random(in: 1...3)
-    
-    switch Int(input ?? "a") {
-    case 1:
-        if comChoice == 1 {
-            print("비겼습니다!")
-        } else if comChoice == 2 {
-            print("졌습니다!")
-        }else if comChoice == 3 {
-            print("이겼습니다!")
-        }
-    case 2:
-        if comChoice == 2 {
-            print("비겼습니다!")
-        } else if comChoice == 3 {
-            print("졌습니다!")
-        }else if comChoice == 1 {
-            print("이겼습니다!")
-        }
-    case 3:
-        if comChoice == 3 {
-            print("비겼습니다!")
-        } else if comChoice == 1 {
-            print("졌습니다!")
-        }else if comChoice == 2 {
-            print("이겼습니다!")
-        }
-    case 0:
-        print("종료합니다")
-        break
-    default :
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-    }
-    print("상대방의 선택: \(comChoice)")
+enum Hand: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
 }
-
+enum ResultMassage: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
+    case gameOver = "게임 종료"
+    case start = "가위(1) 바위(2) 보(3)! <종료 : 0> :"
+    case error = "잘못된 입력입니다. 다시 시도해주세요."
+}
