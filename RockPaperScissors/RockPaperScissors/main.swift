bf04bf8c082997044cfd2d8b8aafeeeeb4d55348
wlsdud-dev <hjymail1025@gmail.com>
2023-11-27 13:05:40 +0900

    feat : 사용자 입력, 컴퓨터 랜덤 선택
    사용자 입력값을 받는 기능 추가, 컴퓨터 가위바위보 선택 기능 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,16 +4,25 @@
 //  Copyright © tastycode. All rights reserved.
 //
 
-enum Hand: Int {
+enum Hand: Int, CaseIterable {
     case scissors = 1
     case rock = 2
     case paper = 3
 }
-enum ResultMassage: String {
+enum Massages: String {
     case win = "이겼습니다!"
     case lose = "졌습니다!"
     case draw = "비겼습니다!"
     case gameOver = "게임 종료"
     case start = "가위(1) 바위(2) 보(3)! <종료 : 0> :"
     case error = "잘못된 입력입니다. 다시 시도해주세요."
+}
+func requestMyHand() -> Int? {
+    print("\(Massages.start.rawValue)", terminator: " ")
+    guard let myHandInput = readLine(), let myHandChoice = Int(myHandInput), Hand.scissors.rawValue...Hand.paper.rawValue ~= myHandChoice else { return nil }
+    return myHandChoice
+}
+func requestComHand() -> Int? {
+    guard let comHandChoice = Hand.allCases.randomElement()?.rawValue else { return nil }
+    return comHandChoice
 }
